8b0af6d34ef27479d5fa96e3a993a13fd0e3f064
Roman Semenyshyn <romansemenyshyn@gmail.com>
2023-06-28 10:40:58 +0300

    post some level of the functions implementation reached

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,120 @@
 /**
- * Import function triggers from their respective submodules:
+ * Copyright 2022 Google LLC
  *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const {
+  beforeUserCreated,
+  beforeUserSignedIn,
+  HttpsError,
+} = require("firebase-functions/v2/identity");
+const admin = require("firebase-admin");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp();
+const db = admin.firestore();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// [START v2ValidateNewUser]
+// [START v2beforeCreateFunctionTrigger]
+// Block account creation with any non-acme email address.
+exports.validatenewuser = beforeUserCreated((event) => {
+  // [END v2beforeCreateFunctionTrigger]
+  // [START v2readUserData]
+  // User data passed in from the CloudEvent.
+  const user = event.data;
+  // [END v2readUserData]
+
+  // [START v2domainHttpsError]
+  // Only users of a specific domain can sign up.
+  if (!user.email.includes("@skf.com")) {
+    // Throw an HttpsError so that Firebase Auth rejects the account creation.
+    throw new HttpsError("invalid-argument", "Unauthorized email");
+  }
+  // [END v2domainHttpsError]
+});
+// [END v2ValidateNewUser]
+
+// [START v2CheckForBan]
+// [START v2beforeSignInFunctionTrigger]
+// Block account sign in with any banned account.
+exports.checkforban = beforeUserSignedIn(async (event) => {
+  // [END v2beforeSignInFunctionTrigger]
+  // [START v2readEmailData]
+  // Email passed from the CloudEvent.
+  const email = event.data.email || "";
+  // [END v2readEmailData]
+
+  // [START v2documentGet]
+  // Obtain a document in Firestore of the banned email address.
+  const doc = await db.collection("banned").doc(email).get();
+  // [END v2documentGet]
+
+  // [START v2bannedHttpsError]
+  // Checking that the document exists for the email address.
+  if (doc.exists) {
+    // Throw an HttpsError so that Firebase Auth rejects the account sign in.
+    throw new HttpsError("invalid-argument", "Unauthorized email");
+  }
+  // [END v2bannedHttpsError]
+});
+// [START v2CheckForBan]
+
+const functions = require("firebase-functions");
+
+exports.newUserSignup = functions.auth.user().onCreate((user) => {
+  console.log("user created", user.email, user.uid);
+  return db.collection("users").doc(user.uid).set({
+    email: user.email,
+    userDisplayName: user.displayName,
+    userCompany: null,
+    userDepartment: null,
+    userSubDepartment: null,
+    userJobRole: [],
+    userReadLevel: 0,
+    userWriteLevel: 0,
+    restApiUrl: null,
+    userDevicesTokens: [],
+    userPasswordSalt: user.passwordSalt,
+    userPasswordHash: user.passwordHash,
+  });
+});
+
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+  console.log("user created", user.email, user.uid);
+  const doc = db.collection("users").doc(user.uid);
+  return doc.delete();
+});
+
+exports.addRequest = functions.https.onCall(async (data, context) => {
+  if (!context.auth.uid) {
+    throw new functions.https.HttpsError(
+        "unauthenticated",
+        "only authenticated users can add request",
+    );
+  }
+  if (data.userJobRole.length > 30) {
+    throw new functions.https.HttpsError(
+        "invalid-argument",
+        "request must be no more than 30 characters long",
+    );
+  }
+  await db.collection("requests").add({
+    text: data.userJobRole,
+    upvote: 0,
+  })
+      .then((doc) => {
+        return {
+          result: doc,
+        };
+      });
+});
