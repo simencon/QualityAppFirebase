1c2eb874a16f2fbcd667c29fac991143cb1eb2dc
Roman Semenyshyn <romansemenyshyn@gmail.com>
2023-07-12 15:45:22 +0300

    Stage before making proper form for users model

--- functions/index.js
+++ functions/index.js
@@ -48,18 +48,23 @@ exports.newUserSignup = functions.auth.user().onCreate((user) => {
       .collection("users")
       .doc(user.uid)
       .set({
+        userId: -1,
         email: user.email,
-        userDisplayName: user.displayName,
-        userCompany: null,
-        userDepartment: null,
-        userSubDepartment: null,
-        userJobRole: [],
-        userReadLevel: 0,
-        userWriteLevel: 0,
+        fullName: user.displayName,
+        companyId: -1,
+        company: null,
+        departmentId: -1,
+        department: null,
+        subDepartmentId: -1,
+        subDepartment: null,
+        jobRoles: [],
+        readLevel: 0,
+        writeLevel: 0,
         restApiUrl: null,
-        userDevicesTokens: [],
-        userPasswordSalt: user.passwordSalt,
-        userPasswordHash: user.passwordHash,
+        userUid: user.uid,
+        devicesTokens: [],
+        passwordSalt: user.passwordSalt,
+        passwordHash: user.passwordHash,
       });
 });
 
@@ -73,57 +78,68 @@ exports.userDeleted = functions.auth.user().onDelete((user) => {
   return doc.delete();
 });
 
-exports.addRequest = functions.https.onCall(async (data, context) => {
-  if (!context.auth.uid) {
-    throw new functions.https.HttpsError(
-        "unauthenticated",
-        "only authenticated users can add request",
-    );
-  }
-  if (data.userJobRole.length > 30) {
-    throw new functions.https.HttpsError(
-        "invalid-argument",
-        "request must be no more than 30 characters long",
-    );
-  }
-  await db.collection("requests").add({
-    text: data.userJobRole,
-    upvote: 0,
-  })
-      .then((doc) => {
-        console.log("doc", doc);
-        return {
-          result: doc,
-        };
-      });
-});
+exports.logUserData = functions
+    .https
+    .onCall(async (data, context) => {
+      if (!context.auth.uid) {
+        throw new functions.https.HttpsError(
+            "unauthenticated",
+            "only authenticated users can add request",
+        );
+      }
+      const snapshot = await db
+          .collection("companies")
+          .doc("skf")
+          .collection("usersInfoLog")
+          .where("email", "==", data.email)
+          .get();
+      if (snapshot.empty) {
+        return db
+            .collection("companies")
+            .doc("skf")
+            .collection("usersInfoLog")
+            .add(
+                data,
+            ).then((record) => {
+              return data;
+            });
+      } else {
+        const updatePromises = [];
+        snapshot.forEach((doc) => {
+          updatePromises.push(
+              db
+                  .collection("companies")
+                  .doc("skf")
+                  .collection("usersInfoLog")
+                  .doc(doc.id)
+                  .update(
+                      data,
+                  ),
+          );
+        });
+        return Promise.all(updatePromises).then((records) => {
+          data.result = "user data logged";
+          return data;
+        });
+      }
+    });
 
-exports.updateUserData = functions.https.onCall( async (data, context) => {
+exports.updateUserData = functions.https.onCall(async (data, context) => {
   if (!context.auth.uid) {
     throw new functions.https.HttpsError(
         "unauthenticated",
         "only authenticated users can add request",
     );
   }
-  if (data.userJobRole.length > 30) {
-    throw new functions.https.HttpsError(
-        "invalid-argument",
-        "request must be no more than 30 characters long",
-    );
-  }
+
   const snapshot = await db.collection("companies")
       .doc("skf")
       .collection("users")
       .where("email", "==", data.email)
       .get();
   const updatePromises = [];
-  const userJobRoles = [];
-  data.userJobRole.forEach( (role) => {
-    console.log("role", role);
-    userJobRoles.push(role);
-  },
-  );
-  snapshot.forEach( (doc) => {
+
+  snapshot.forEach((doc) => {
     updatePromises.push(
         db
             .collection("companies")
@@ -131,24 +147,91 @@ exports.updateUserData = functions.https.onCall( async (data, context) => {
             .collection("users")
             .doc(doc.id)
             .update({
-              userDisplayName: data.userDisplayName,
-              userDepartment: data.userDepartment,
-              userSubDepartment: data.userSubDepartment,
-              userJobRole: [userJobRoles],
+              fullName: data.fullName,
+              department: data.department,
+              subDepartment: data.subDepartment,
+              jobRoles: data.jobRoles,
             }),
     );
-  },
-  );
+  });
+
   return Promise
       .all(updatePromises)
       .then((timeStamp) => {
         return {
           result: "user data updated",
           email: data.email,
-          fullName: data.userDisplayName,
-          department: data.userDepartment,
-          subDepartment: data.userSubDepartment,
-          jobRoles: [userJobRoles],
+          fullName: data.fullName,
+          department: data.department,
+          subDepartment: data.subDepartment,
+          jobRoles: data.jobRoles,
         };
       });
 });
+
+exports.getUserData = functions.https.onCall( (data, context) => {
+  if (!context.auth.uid) {
+    throw new functions.https.HttpsError(
+        "unauthenticated",
+        "only authenticated users can add request",
+    );
+  }
+  return db.collection("companies")
+      .doc("skf")
+      .collection("users")
+      .where("email", "==", data.email)
+      .get()
+      .then((ref) => {
+        return ref.docs.map((doc) => doc.data())[0];
+      });
+});
+
+const axios = require("axios");
+
+exports.createNewTeamMember = functions.https.onCall((data, context) => {
+  if (!context.auth.uid) {
+    throw new functions.https.HttpsError(
+        "unauthenticated",
+        "only authenticated users can add request",
+    );
+  }
+  return axios.post("https://qualityappspring.azurewebsites.net/api/v1/teamMembersTesting", {
+    departmentId: 42,
+    department: "Fucking cool department",
+    email: data.email,
+    fullName: data.email,
+    jobRole: "Big boss",
+    roleLevelId: 1,
+    passWord: "111111",
+    companyId: 1,
+  })
+      .then((response) => {
+        console.log("response code is: ", response.status);
+        console.log("response is: ", response.data);
+        return {email: data.email};
+      })
+      .catch((error) => {
+        console.log("error is: ", error.data);
+      });
+});
+
+exports.createRequestedTeamMember = functions
+    .firestore.document("companies/skf/users/{fullName}")
+    .onUpdate((snap, context) => {
+      const userData = snap.after.data();
+      return axios.post("https://qualityappspring.azurewebsites.net/api/v1/requestedTeamMembers", {
+        department: userData.department,
+        email: userData.email,
+        fullName: userData.fullName,
+        jobRole: userData.jobRoles[0],
+        subDepartment: userData.subDepartment,
+        userUid: userData.userUid,
+      })
+          .then((response) => {
+            console.log("response code is: ", response.status);
+            console.log("response is: ", response.data);
+          })
+          .catch((error) => {
+            console.log("error is: ", error.data);
+          });
+    });
